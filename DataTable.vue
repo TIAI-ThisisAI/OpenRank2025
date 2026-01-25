298d80ddaaf203b0e600cf8897f24a67daba1ef3
Peng99999 <78625977+Peng99999@users.noreply.github.com>
2026-01-25 22:44:40 +0800

    add the main
    入口文件，初始化 Vue 实例并挂载到 DO

--- DataTable.vue
+++ DataTable.vue
@@ -156,3 +156,13 @@ export default {
 }
 </style>
 
+import Vue from 'vue';
+import App from './App.vue';
+
+Vue.config.productionTip = false;
+
+new Vue({
+  render: h => h(App),
+}).$mount('#app');
+
+

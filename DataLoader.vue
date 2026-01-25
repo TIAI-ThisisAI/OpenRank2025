043c01577a34ac082a45e462b7b8ddf0a3816c75
Peng99999 <78625977+Peng99999@users.noreply.github.com>
2026-01-25 22:36:32 +0800

    Update DataLoader.vue

--- DataLoader.vue
+++ DataLoader.vue
@@ -79,3 +79,43 @@ th, td {
   text-align: left;
 }
 </style>
+
+<template>
+  <div class="chart-container">
+    <canvas ref="chart"></canvas>
+  </div>
+</template>
+
+<script>
+import { Chart } from 'chart.js';
+
+export default {
+  props: {
+    chartData: Object,
+    chartOptions: Object,
+  },
+  mounted() {
+    this.renderChart();
+  },
+  methods: {
+    renderChart() {
+      if (this.chartData && this.chartOptions) {
+        new Chart(this.$refs.chart, {
+          type: 'line',  // Chart type (line, bar, etc.)
+          data: this.chartData,
+          options: this.chartOptions,
+        });
+      }
+    },
+  },
+};
+</script>
+
+<style scoped>
+.chart-container {
+  position: relative;
+  height: 400px;
+  width: 100%;
+}
+</style>
+

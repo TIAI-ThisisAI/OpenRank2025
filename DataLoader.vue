ff6664aa19089f3d28eeb95db694aa08ae9306bb
Peng99999 <78625977+Peng99999@users.noreply.github.com>
2026-01-25 22:39:13 +0800

    Update DataLoader.vue

--- DataLoader.vue
+++ DataLoader.vue
@@ -146,4 +146,14 @@ export default {
 }
 </style>
 
+import Vue from 'vue';
+import App from './App.vue';
+
+Vue.config.productionTip = false;
+
+new Vue({
+  render: h => h(App),
+}).$mount('#app');
+
+
 

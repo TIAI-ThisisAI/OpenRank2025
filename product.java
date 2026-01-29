6a7a302907d011636fbd22b6fdabe1af93e47415
Peng99999 <78625977+Peng99999@users.noreply.github.com>
2026-01-29 10:36:29 +0800

    Add Product Category

--- product.java
+++ product.java
@@ -251,6 +251,43 @@ public class ProductManagerCLI {
     }
 }
 
+public class Category {
+    private String name;
+    private String description;
+
+    // Constructor
+    public Category(String name, String description) {
+        this.name = name;
+        this.description = description;
+    }
+
+    // Getters and Setters
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    @Override
+    public String toString() {
+        return "Category{" +
+                "name='" + name + '\'' +
+                ", description='" + description + '\'' +
+                '}';
+    }
+}
+
+
 
 
 
